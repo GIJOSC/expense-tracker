30c294c00aacc93fcc41dddbccada62e3e82ed3b
gijosc <neves.gesiel@gmail.com>
2023-01-04 21:01:19 -0300

    feat: Created project and components

---
+++ src/data/items.ts
@@ -0,0 +1,34 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 1, 4),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2023, 1, 4),
+    category: "food",
+    title: "Burguer King",
+    value: 34,
+  },
+  {
+    date: new Date(2023, 1, 3),
+    category: "rent",
+    title: "Home",
+    value: 2.5,
+  },
+  {
+    date: new Date(2023, 1, 2),
+    category: "food",
+    title: "Da Dula",
+    value: 120.12,
+  },
+  {
+    date: new Date(2023, 1, 4),
+    category: "salary",
+    title: "Salário GIJO",
+    value: 5000,
+  },
+];
