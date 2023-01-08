3edbff53c3401063a669288db0d8c583e49575b7
gijosc <neves.gesiel@gmail.com>
2023-01-08 16:18:23 -0300

    feat: Add format to date

--- src/data/items.ts
+++ src/data/items.ts
@@ -2,31 +2,31 @@ import { Item } from "../types/Item";
 
 export const items: Item[] = [
   {
-    date: new Date(2023, 1, 4),
+    date: new Date(2023, 1, 8),
     category: "food",
     title: "McDonalds",
     value: 32.12,
   },
   {
-    date: new Date(2023, 1, 4),
+    date: new Date(2023, 1, 8),
     category: "food",
     title: "Burguer King",
     value: 34,
   },
   {
-    date: new Date(2023, 1, 3),
+    date: new Date(2023, 1, 8),
     category: "rent",
     title: "Home",
     value: 2.5,
   },
   {
-    date: new Date(2023, 1, 2),
+    date: new Date(2023, 1, 8),
     category: "food",
     title: "Da Dula",
     value: 120.12,
   },
   {
-    date: new Date(2023, 1, 4),
+    date: new Date(2022, 12, 18),
     category: "salary",
     title: "Salário GIJO",
     value: 5000,
