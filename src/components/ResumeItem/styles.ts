4f016e4e4cb15f0c4bd82a218a6a9a1f0527bdc7
gijosc <neves.gesiel@gmail.com>
2023-01-08 23:47:53 -0300

    feat: Add Count in expense and income

--- src/components/ResumeItem/styles.ts
+++ src/components/ResumeItem/styles.ts
@@ -4,6 +4,15 @@ export const Container = styled.div`
   flex: 1;
 `;
 
-export const Title = styled.div``;
+export const Title = styled.div`
+  text-align: center;
+  font-weight: bold;
+  color: #888;
+  margin-bottom: 5px;
+`;
 
-export const Info = styled.div``;
+export const Info = styled.div`
+  text-align: center;
+  font-weight: bold;
+  color: #000;
+`;
